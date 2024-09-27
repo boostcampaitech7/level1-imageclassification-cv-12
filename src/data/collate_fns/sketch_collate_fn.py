ed9665c4b9ce646fdbd0b99714675d1a9e13c5e9
seungminchung <taky0315@naver.com>
2024-09-27 11:16:26 +0900

    [ add ] add code

--- src/data/collate_fns/sketch_collate_fn.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-# 데이터셋의 배치를 결정하는 모듈이다.
-# 특별한 커스텀을 하지 않고 단순히 기본적인 배치 처리를 진행하지만 후에 배치 처리시 커스텀이 가능하도록 모둘화 되어있다.
-def sketch_collate_fn(batch):
-    batch = torch.utils.data.dataloader.default_collate(batch)
-    return batch
